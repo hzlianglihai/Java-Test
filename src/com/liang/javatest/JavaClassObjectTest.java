86c5ca7a175ec7e5e094e1ff925c10577c8ebb3c
梁立海 <hzlianglihai@corp.netease.com>
2017-11-22 11:40:59 +0800

    test

--- src/com/liang/javatest/JavaClassObjectTest.java
+++ src/com/liang/javatest/JavaClassObjectTest.java
@@ -1,7 +1,50 @@
 package com.liang.javatest;
 
-public class JavaClassObjectTest {
-public static void main(String[] args) {
-	
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+class Toy {
+	Toy() {
+		System.out.println("Toy constructor");
+	}
+
+	Toy(int i) {
+		System.out.println("Toy constructor : " + i);
+	}
+
 }
+
+class FancyToy extends Toy {
+	FancyToy() {
+		super(1);
+	}
+}
+public class JavaClassObjectTest {
+
+	static void printinfo(Class cc) {
+		System.out.println("Class name : " + cc.getName() + " is Interface : " + cc.isInterface());
+	}
+
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+		Class c = null;
+		try {
+			c = Class.forName("com.liang.javatest.FancyToy");
+			Class up = c.getSuperclass();
+			//??
+			Class down = c.getClass();
+			printinfo(c);
+			//??
+			printinfo(down);
+			printinfo(up);
+			printinfo(FancyToy.class);
+			Method[] methods = FancyToy.class.getMethods();
+			for(Method method : methods) {
+				System.out.println(method.getName() + method.getParameterTypes());
+				//??
+//				method.invoke(c.newInstance(), args);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
